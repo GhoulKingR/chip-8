bcbb690b49325ea9eba2d5d7fa29aeb134818d04
Chigozie Oduah <oduahchigozie46@gmail.com>
2026-03-12 21:07:03 +0100

    new config files

---
+++ config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+static const int DISPLAY_WIDTH  = 512;      // scale 8 : 1
+static const int DISPLAY_HEIGHT = 256;      // scale 8 : 1
+static const int RAMSIZE        = 4096;
+
+#define SEND_ERR(str, ...) \
+    fprintf(stderr, "Error: " str "\n" __VA_OPT__(,) __VA_ARGS__);
+
+#define SEND_FAILED(...) \
+    SEND_ERR(__VA_ARGS__);\
+    exit(EXIT_FAILURE);
+
+#ifdef DEBUG
+#define DEBUG_LOG(str, ...) \
+    printf("Debug: " str "\n" __VA_OPT__(,) __VA_ARGS__)
+#else
+#define DEBUG_LOG(str, ...) 0
+#endif
